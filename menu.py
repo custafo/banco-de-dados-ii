f4db45349eee188372c22761adeac466cbedfcdb
Luís Gustavo de Oliveira Pereira <93691188+luisgustavoft@users.noreply.github.com>
2024-11-06 22:37:06 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,59 +1,106 @@
-
-
 def main_menu():
-    print("\nMenu de Navegação do Sistema de Banco de Dados para Restaurante")
-    print("1. Criar Banco de Dados")
-    print("2. Destruir Banco de Dados")
-    print("3. Inserir Dados em Cliente")
-    print("4. Consultar Vendas")
-    print("5. Listar Procedimentos")
-    print("6. Listar Funcoes")
-    print("7. Listar Triggers")
-    print("8. Sair")
-    
-    try:
-        choice = int(input("\nSelecione uma opcoo: "))
-        return choice
-    except ValueError:
-        print("Entrada invalida. Insira um número correspondente a opcao.")
-        return None
+    while True:
+        print("\n--- Bem vindo à database Restaurante, efetue o login para continuar ---")
+        print("digite a opção desejada:")
+        print("1. logar como administrador ")
+        print("2. logar como gerente")
+        print("3. logar como funcionário")
+        print("4. Sair do programa")
+        
+        choice = input("Selecione uma opção (1-4): ")
 
-def handle_option(choice, engine):
-    if choice == 1:
-        create_database(engine)
-        print("Banco de dados criado.")
-    elif choice == 2:
-        drop_database(engine)
-        print("Banco de dados destruido.")
-    elif choice == 3:
-        nome = input("Nome do cliente: ")
-        sexo = input("Sexo (m/f/o): ")
-        idade = input("Idade: ")
-        nascimento = input("Data de nascimento (YYYY-MM-DD): ")
-        pontos = input("Pontos iniciais: ")
-        insert_cliente(engine, nome, sexo, idade, nascimento, pontos)
-        print("Cliente inserido.")
-    elif choice == 4:
-        vendas = select_venda(engine)
-        print("Vendas:", vendas)
-    elif choice == 5:
-        print("Procedimentos disponiveis: reajuste, sorteio, estatisticas, gastar_pontos")
-    elif choice == 6:
-        print("Funcoes disponiveis: calculo de pontos")
-    elif choice == 7:
-        print("Triggers implementados: calculo de pontos por gasto, expiracao de ingredientes, disponibilidade de pratos e atualizacao de estoque")
-    elif choice == 8:
-        print("Saindo do sistema...")
-        return False
-    else:
-        print("Opcao invalida. Tente novamente.")
-    return True
+        if choice == '1':
+            administrador_menu()
+        elif choice == '2':
+            gerente_menu()
+        elif choice == '3':
+            funcionario_menu()
+        elif choice == '4':
+            print("Saindo do programa")
+            break
+        else:
+            print("Seleção inválida, tente novamente")
+            print()
 
-def run_menu(engine):
-    running = True
-    while running:
-        choice = main_menu()
-        if choice is not None:
-            running = handle_option(choice, engine)
+def funcionario_menu():
+    while True:
+        print("\n--- bem vindo, funcionário! ---")
+        print("digite a opção desejada:")
+        print("1. Cadastrar novo item")
+        print("2. consultar vendas")
+        print("3. log-off")
+        
+        choice = input("Select an option (1-3): ")
 
-run_menu(engine)
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            print("deslogando")
+            main_menu()
+            break
+        else:
+            print("Seleção inválida, tente novamente")
+
+def gerente_menu():
+    while True:
+        print("\n--- bem vindo, gerente! ---")
+        print("digite a opção desejada:")
+        print("1. Cadastrar novo item")
+        print("2. consultar uma tabela")
+        print("3. apagar um registro")
+        print("4. editar um registro existente")
+        print("5. log-off")
+        
+        choice = input("Select an option (1-3): ")
+
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            print("deslogando")
+            main_menu()
+            break
+        else:
+            print("Seleção inválida, tente novamente")
+
+def administrador_menu():
+    while True:
+        print("\n--- bem vindo, administrador! ---")
+        print("digite a opção desejada:")
+        print("1. Cadastrar novo item")
+        print("2. consultar uma tabela")
+        print("3. apagar um registro")
+        print("4. editar um registro existente")
+        print("6. destruir database")
+        print("7. demonstrações de requerimentos")
+        print("8. log-off")
+        
+        choice = input("Selecione uma opção (1-8)): ")
+
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            pass
+        elif choice == '6':
+            pass
+        elif choice == '7':
+            pass
+        elif choice == '8':
+            print("deslogando")
+            main_menu()
+            break
+        else:
+            print("Seleção inválida, tente novamente")
